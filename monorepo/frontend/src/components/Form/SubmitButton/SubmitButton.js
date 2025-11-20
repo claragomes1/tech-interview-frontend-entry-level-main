9c42dd4f69cf42218366cfe8fb606c22ebe3b29e
claragomes1 <claraloris39@gmail.com>
2025-11-20 15:38:15 -0300

    feat: enhance SubmitButton component with disabled state handling

--- monorepo/frontend/src/components/Form/SubmitButton/SubmitButton.js
+++ monorepo/frontend/src/components/Form/SubmitButton/SubmitButton.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-function SubmitButton({ text }) {
-  return <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">{text}</button>;
+function SubmitButton({ text, isDisabled }) {
+  return (
+    <button
+      type="submit"
+      className={`mt-6 text-white font-bold py-2 px-4 rounded-xl bg-[#07151E] ${
+        isDisabled
+          ? 'bg-blue-500 opacity-50 cursor-not-allowed pointer-events-none'
+          : 'bg-blue-500 hover:bg-[#102535]'
+      }`}
+      disabled={isDisabled}
+      aria-disabled={isDisabled}
+    >
+      {text}
+    </button>
+  );
 }
 
 export default SubmitButton;
